da68dcd6aa7c047bc4f9ac1a0ed3aa9195f0b95c
Jingtao Li <35481911+zlijingtao@users.noreply.github.com>
2018-03-28 13:37:02 +0800

    Update Run_SAR_ccliu.m

--- Run_SAR_ccliu.m
+++ Run_SAR_ccliu.m
@@ -1,6 +1,6 @@
 clear all
 clc
-n=14;%The resolution must be an even number
+n=14;%This n must be an even number and usually larger than 4.
 [adco,energy]=SAR_ccliu(n)
-[SNDR,SFDR,ENOB,SNR]=SNR_ADC_N(adco,n)
+[SNDR,SFDR,ENOB,SNR]=SNR_ADC(adco,n)
 energy
