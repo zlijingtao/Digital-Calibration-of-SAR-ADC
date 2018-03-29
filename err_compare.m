bc6a85e8d49717e811f32483a2543ae9f3c4c331
Jingtao Li <35481911+zlijingtao@users.noreply.github.com>
2018-03-29 17:10:45 +0800

    revise a fatal error

--- err_compare.m
+++ err_compare.m
@@ -4,7 +4,7 @@ function output=err_compare(i,j,error_rate)
 %error_rate is a float number between 0 and 1. i.e.: 0.10.
 %i,j are two numbers which needs for comparison.
 %the output of this function is either "1" or "0".
-q=rand(0,1);
+q=rand(1,1);
 if i<j
 if q<error_rate
     output=1;
